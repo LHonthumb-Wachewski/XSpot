c57003312ebf5fe4ace02e3c943ebb95e5ec77d7
Liam Honthumb-Wachewski <120189862+LHonthumb-Wachewski@users.noreply.github.com>
2023-09-05 08:54:36 -0400

    jooooooooooooooooooooooooooooooooooooo

---
+++ XSpot.rb
@@ -0,0 +1,15 @@
+class Player
+    def initialize(name, hp, mp, atk, dff, eva, spd, CR)
+        @name = name
+        @hp = hp
+        @mp = mp
+        @atk = atk
+        @dff = dff
+        @eva = eva
+        @spd = spd
+        @CR = CR
+        @LV = 1
+        @EXP = 0
+    def test
+        puts "Hello"
+Player.test
